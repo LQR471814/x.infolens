51793993e2f39887193274955f93d74d098e8158
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-03-09 22:23:27 -0700

    make changes

--- firecrawl/start.sh
+++ firecrawl/start.sh
@@ -1,11 +1,21 @@
 # ===== Required ENVS ======
+# export NUM_WORKERS_PER_QUEUE=8
+# export HOST=127.0.0.1
+
 export NUM_WORKERS_PER_QUEUE=8
 export PORT=3002
 export HOST=0.0.0.0
 export REDIS_URL=redis://localhost:6379
 export REDIS_RATE_LIMIT_URL=redis://localhost:6379
-
-## To turn on DB authentication, you need to set up supabase.
+export PLAYWRIGHT_MICROSERVICE_URL=http://localhost:3000/html
 export USE_DB_AUTHENTICATION=false
 
-cd repo && docker compose up
+dir=$(pwd)
+cd $dir/repo/apps/playwright-service-ts && pnpm install
+cd $dir/repo/apps/playwright-service-ts && pnpm exec playwright install
+cd $dir/repo/apps/playwright-service-ts && pnpm run build
+cd $dir/repo/apps/api && pnpm install
+cd $dir
+
+mprocs -c ./mprocs.yaml
+
